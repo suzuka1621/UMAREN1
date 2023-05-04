c0678a60f81f2fdce2d33758f60f46093fcdeab2
suzuka1621 <akaneakua0625@gmail.com>
2023-05-05 00:16:07 +0900

    Javascriptの記述修正

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -20,10 +20,10 @@ const appChatRoom = consumer.subscriptions.create("ChatRoomChannel", {
 });
 
   if(/chat_rooms/.test(location.pathname)) {
-   $(document).on("keydown", ".chat-room__message-form_textarea", function(e) {
-    if (e.key === "Enter") {
-      const chat_room_id = $('textarea').data('chat_room_id')
-      appChatRoom.speak(e.target.value);
+    $(document).on("keydown", ".chat-room__message-form_textarea", function(e) {
+      if (e.key === "Enter") {
+        const chat_room_id = $('textarea').data('chat_room_id')
+        appChatRoom.speak(e.target.value, chat_room_id);
       e.target.value = '';
       e.preventDefault();
     }
