9a617f451796758bd3b3982d9b9fbe77c4b46dc4
suzuka1621 <akaneakua0625@gmail.com>
2023-05-04 18:03:26 +0900

    ビューファイルの作成

--- app/controllers/matching_controller.rb
+++ app/controllers/matching_controller.rb
@@ -1,4 +1,4 @@
-class MatchingsController < ApplicationController
+class MatchingController < ApplicationController
 
   before_action :authenticate_user!
 
--- test/controllers/matching_controller_test.rb
+++ test/controllers/matching_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class MatchingsControllerTest < ActionDispatch::IntegrationTest
+class MatchingControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
