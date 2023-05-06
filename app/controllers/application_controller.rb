c2d01b0d236373c9ffe3dfe4a235ef9110461a3c
suzuka1621 <akaneakua0625@gmail.com>
2023-05-07 02:15:40 +0900

    誕生日の表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :gender])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :gender, :birthday])
 
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :self_introduction, :prefecture_id, :favorite_genre, :profile_image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :self_introduction, :prefecture_id, :favorite_genre, :profile_image, :birthday])
   end
 
 end
